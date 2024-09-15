6ade72f4e90f5d268cba4ab13859d881961b173c
vladimir <vladimir.gomes@megamamute.com.br>
2024-09-14 21:10:17 -0300

    First deploy

---
+++ contrato.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from typing import Tuple
+
+from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt
+from enum import Enum
+
+class ProdutoEnum(str, Enum):
+    produto1 = "ZapFlow com Gemini"
+    produto2 = "ZapFlow com chatGPT"
+    produto3 = "ZapFlow com Llama3.0"
+
+class Vendas(BaseModel):
+    email: EmailStr
+    data: datetime
+    valor: PositiveFloat
+    quantidade: PositiveInt
+    produto: ProdutoEnum
